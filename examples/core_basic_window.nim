545620544c60faf15013639f2d17e3082ac81881
Justin Hurstwright <jryanhurst@gmail.com>
2019-10-25 20:29:46 -0700

    Create Base Window

--- examples/core_basic_window.nim
+++ examples/core_basic_window.nim
@@ -1,5 +1,11 @@
-import nimraylib/raylib
+import nimraylib
 
 InitWindow(800, 450, "raylib [core] examples - basic window")
+
+while not WindowShouldClose():
+  BeginDrawing()
+  ClearBackground(Raywhite)
+  DrawText("Congrats! You create first window!", 190, 200, 20, Lightgray)
+  EndDrawing()
 
 CloseWindow()
